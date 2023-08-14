0f63036da369d778a9c9822acf16046159c4620f
KarissaTysklind <112622524+KarissaTysklind@users.noreply.github.com>
2023-08-14 18:30:12 +0200

    modified:   c3-website.tf 	deleted:    website-files/Site/js/desktop.ini

--- c3-website.tf
+++ c3-website.tf
@@ -59,10 +59,67 @@ resource "aws_s3_bucket_policy" "website" {
   })
 }
 
-# Website S3 Bucket Files
 resource "aws_s3_object" "uploadfiles" {
-    for_each = fileset("./website-files/Site/", "**")
-    bucket = aws_s3_bucket.website.id
-    key = each.value
-    source = "./website-files/Site/${each.value}" 
+  for_each = fileset("./website-files/Site/", "**")
+  bucket   = aws_s3_bucket.website.id
+  key      = each.value
+  source   = "./website-files/Site/${each.value}"
 }
+
+# CloudFront Configuration
+resource "aws_cloudfront_distribution" "website_distribution" {
+  aliases = []
+
+  comment             = null
+  enabled             = true
+  is_ipv6_enabled     = true
+  price_class         = "PriceClass_100"
+  retain_on_delete    = false
+  wait_for_deployment = true
+
+  default_root_object = "index.html"
+
+  origin {
+    domain_name         = "${var.s3_bucket_name}.s3-website-${var.aws_region}.amazonaws.com"
+    origin_id           = "${var.s3_bucket_name}.s3-website-${var.aws_region}.amazonaws.com"
+    connection_attempts = 3
+    connection_timeout  = 10
+
+    custom_origin_config {
+      http_port                = 80
+      https_port               = 443
+      origin_keepalive_timeout = 5
+      origin_read_timeout      = 30
+      origin_ssl_protocols     = ["TLSv1.2"]
+      origin_protocol_policy   = "http-only"
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      locations        = []
+      restriction_type = "none"
+    }
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "${var.s3_bucket_name}.s3-website-${var.aws_region}.amazonaws.com"
+    viewer_protocol_policy = "redirect-to-https"
+    cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+    compress        = true
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+    acm_certificate_arn            = ""
+    ssl_support_method             = "sni-only"
+  }
+}
+
+
+
+        
